cb35627c27c79824ebbe94463c2baa062893fff2
Marina <marina@Marinas-MacBook-Air.local>
2019-11-05 00:35:26 -0500

    Added POST and DELETE request functionalities

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let express = require('express')
-let app = express()
-let teams = require('./teams.json')
+const express = require('express')
+const app = express()
+const teams = require('./teams.json')
+const bodyParser = require('body-parser')
 
 app.get('/teams', (request, response) => {
     response.send(teams)
@@ -14,10 +15,42 @@ app.get('/teams/:filter', (request, response) => {
     response.send(team)
 })
 
+app.post('/teams', bodyParser.json(), (request, response) => {
+    const { id, location, mascot, abbreviation, conference, division } = request.body
+
+    if (!location || !mascot || !abbreviation || !conference || !division) {
+        response.send('The following attributes are required: location, mascot, abbreviation, conference and division.')
+    }
+
+    const newTeam = { id, location, mascot, abbreviation, conference, division }
+
+    if (!id) {
+        newTeam.id = teams.length + 1;
+    }
+
+    teams.push(newTeam)
+    console.log({newTeam})
+    response.send(newTeam)
+})
+
+app.delete('/teams/:id', (request, response) => {
+    // Look up the team
+    // If it doesn't exist, return 404
+    const team = teams.find(t => t.id === parseInt(request.params.id));
+    if (!team) response.status(404).send('The team with the given ID does not exist.')
+
+    // Otherwise, delete it
+    const index = teams.indexOf(team);
+    teams.splice(index, 1);
+
+    // Return the same team
+    response.send('Successfully deleted the team with ID: ' + team.id)
+})
+
 app.all('*', (request, response) => {
     response.send('404 - Not Found. Please be more specific.')
 })
 
-app.listen(1337, () => {
-    console.log('Server is up and running')
+app.listen(5500, () => {
+    console.log('Server is up and running.')
 })
