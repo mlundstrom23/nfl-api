1cb452be43cacbd2fe4e8de7baaeb4e402987908
Marina <marina@marinas-air.worcester.local>
2019-11-16 14:21:56 -0500

    Added async and await

--- config/sequelize.js
+++ config/sequelize.js
@@ -6,6 +6,6 @@ module.exports = {
         database: process.env.DB_NAME,
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
-        dialect: 'mysql',
+        dialect: process.env.DB_DIALECT
     }
 }
--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require('express')
 
-// todo: create the teams.js model
-const TeamsModel = require('./models/teams')
-
-// Creating an instance of an Express application
-const app = express()
-
 // Instead of const teams = require('./teams.json') because it will only give you results in that json file
 const models = require('./models')
 
 // Middleware that takes a string and turns it into a JS object out of that string that we can use
 const bodyParser = require('body-parser')
+
+// Operator that can be used to make more complex comparisons
 const Op = require('sequelize').Op
 
-app.get('/teams', (request, response) => {
-    models.Teams.findAll({ include: { model: models.Teams } }).then((teams) => {
-        response.send(teams)
-    })
+// Creating an instance of an Express application
+const app = express()
+
+app.get('/teams', async (request, response) => {
+    const teams = await models.Teams.findAll()
+    response.send(teams)
 })
 
-app.get('/teams/:filter', (request, response) => {
+app.get('/teams/:filter', async (request, response) => {
+    // Short-hand for const filter = request.params.filter
     const { filter } = request.params
-    const match = models.Teams.findOne({
+    const match = await models.Teams.findOne({
         where: { [Op.or]: [{ id: filter },
                            { abbreviation: filter },
                            { division: filter }] }
@@ -33,7 +32,7 @@ app.get('/teams/:filter', (request, response) => {
     }
 })
 
-app.post('/teams', bodyParser.json(), (request, response) => {
+app.post('/teams', bodyParser.json(), async (request, response) => {
     const body = request.body
 
     if (
@@ -46,11 +45,11 @@ app.post('/teams', bodyParser.json(), (request, response) => {
       response.status(400).send('The following attributes are required: location, mascot, abbreviation, conference and division.')
     }
 
-    const newTeam = models.Teams.create({ location,
-                                          mascot,
-                                          abbreviation,
-                                          conference,
-                                          division })
+    const newTeam = await models.Teams.create({ location,
+                                                mascot,
+                                                abbreviation,
+                                                conference,
+                                                division })
 
     response.status(201).send(newTeam);
 })
