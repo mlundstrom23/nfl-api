4f8fa67b9010f0a3753478b9d569e68a5d8b3768
Marina <marina@Marinas-MacBook-Air.local>
2019-11-15 21:29:23 -0500

    Updated API to use new database instead of JSON file

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 const express = require('express')
 
+// todo: create the teams.js model
+const TeamsModel = require('./models/teams')
+
 // Creating an instance of an Express application
 const app = express()
-const teams = require('./teams.json')
+
+// Instead of const teams = require('./teams.json') because it will only give you results in that json file
+const models = require('./models')
 
 // Middleware that takes a string and turns it into a JS object out of that string that we can use
 const bodyParser = require('body-parser')
+const Op = require('sequelize').Op
 
 app.get('/teams', (request, response) => {
-    response.send(teams)
+    models.Teams.findAll({ include: { model: models.Teams } }).then((teams) => {
+        response.send(teams)
+    })
 })
 
 app.get('/teams/:filter', (request, response) => {
-    let filter = request.params.filter
-    let result = teams.filter((team) => {
-        
-        // Or else String(team.id) === filter
-        return team.id == filter || 
-               team.abbreviation === filter ||
-               team.division === filter                  
+    const { filter } = request.params
+    const match = models.Teams.findOne({
+        where: { [Op.or]: [{ id: filter },
+                           { abbreviation: filter },
+                           { division: filter }] }
     })
-
-    // Just sends back locations when inputting any of the fields above
-    /*let locations = result.map((team) => {
-        return team.location
-    })*/
-    response.send(result) // response.send(locations) to just send back location and uncomment lines 25-27
-    console.log(filter)
+    if (match) {
+        response.send(match) 
+    } else {
+        response.status(404).send('Please provide a valid id, abbreviation or division to look up team.')
+    }
 })
 
 app.post('/teams', bodyParser.json(), (request, response) => {
     const body = request.body
-
-    if (!body.id) {
-        body.id = teams.length + 1
-    }
 
     if (
        !body.location || 
@@ -46,32 +46,13 @@ app.post('/teams', bodyParser.json(), (request, response) => {
       response.status(400).send('The following attributes are required: location, mascot, abbreviation, conference and division.')
     }
 
-    const newTeam = teams.concat(body)
+    const newTeam = models.Teams.create({ location,
+                                          mascot,
+                                          abbreviation,
+                                          conference,
+                                          division })
 
-    console.log({body})
-    response.send(newTeam)
-})
-
-app.delete('/teams/:id', (request, response) => {
-    // Look up the team
-    // If it doesn't exist, return 404
-    const team = teams.find(t => t.id === parseInt(request.params.id));
-
-    if (!team) {
-        response.status(404).send('The team with the given ID does not exist.')
-
-    } else {
-    // Otherwise, delete it
-        const index = teams.indexOf(team);
-        teams.splice(index, 1);
-    }
-
-    // Return successful delete attempt message
-    response.send('Successfully deleted the team with ID: ' + team.id)
-})
-
-app.all('*', (request, response) => {
-    response.send('404 - Not Found. Please be more specific.')
+    response.status(201).send(newTeam);
 })
 
 app.listen(1337, () => {
